419d1c2f8a76ae999fb136ebf88c2222c82b37e3
Scott Hannen <shannen@signifyhealth.com>
2022-12-01 15:14:23 -0500

    Add project files.

---
+++ AdventOfCode2022/Day1/Day1.cs
@@ -0,0 +1,35 @@
+namespace AdventOfCode2022.Day1
+{
+    public class Day1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var result = GetLists();
+            var caloriesByList = result.Select(list => list.Sum());
+            var caloriesByListSorted = caloriesByList.OrderByDescending(c => c);
+            var top3 = caloriesByListSorted.Take(3);
+            var top3Total = top3.Sum();
+        }
+
+        private List<List<int>> GetLists()
+        {
+            var result = new List<List<int>>();
+            result.Add(new List<int>());
+            var lines = File.ReadAllLines("day_1_input.txt");
+            foreach (var line in lines)
+            {
+                if (int.TryParse(line, out int calories))
+                {
+                    result.Last().Add(calories);
+                }
+                else
+                {
+                    result.Add(new List<int>());
+                }
+            }
+
+            return result;
+        }
+    }
+}
